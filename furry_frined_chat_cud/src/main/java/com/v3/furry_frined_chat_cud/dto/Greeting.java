d03a3d76643da44fc29804cb2dae75935e6500d0
youngsoosoo <lovepys01@naver.com>
2023-07-06 16:00:52 +0900

    WebSocket + STOMP를 통한 학습

---
+++ furry_frined_chat_cud/src/main/java/com/v3/furry_frined_chat_cud/dto/Greeting.java
@@ -0,0 +1,18 @@
+package com.v3.furry_frined_chat_cud.dto;
+
+public class Greeting {
+
+    private String content;
+
+    public Greeting() {
+    }
+
+    public Greeting(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+}
